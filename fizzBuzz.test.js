e67ccf644b75e0d271523642e20daac0fe4fbc0a
Clare Coates <89577030+CCoates46@users.noreply.github.com>
2023-08-04 15:09:59 +0100

    Added more tests to cover new functionality

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -16,13 +16,30 @@ describe('test suite for fizzBuzz kata', () => {
         expect(fizzBuzz(6)).toBe('Fizz')
     });
 
+    it('should return Fizz if the input contains a 3', () => {
+        expect(fizzBuzz(3)).toBe('Fizz')
+        expect(fizzBuzz(13)).toBe('Fizz')
+    });
+
     it('should return Buzz if the input is divisible by 5', () => {
         expect(fizzBuzz(5)).toBe('Buzz')
         expect(fizzBuzz(10)).toBe('Buzz')
     });
 
+    it('should return Buzz if the input contains a 5', () => {
+        expect(fizzBuzz(5)).toBe('Buzz')
+        expect(fizzBuzz(25)).toBe('Buzz')
+    });
+
+    it('should return FizzBuzz if the input contains a 3 and a 5', () => {
+        expect(fizzBuzz(53)).toBe('FizzBuzz')
+    });    
+
     it('should return FizzBuzz if the input is divisible by 3 and 5', () => {
         expect(fizzBuzz(15)).toBe('FizzBuzz')
         expect(fizzBuzz(30)).toBe('FizzBuzz')
+    });
+    it('should return FizzBuzzBuzz if the input contains 3 and 5 and is divisible by 5', () => {
+        expect(fizzBuzz(35)).toBe('FizzBuzzBuzz')
     });
 });
