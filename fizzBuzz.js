080e996eacf65e16bfd1771b7fb5c4d8cf4eb05b
Clare Coates <89577030+CCoates46@users.noreply.github.com>
2023-08-04 21:09:18 +0100

    refactored code to include 2 new functions

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,9 +1,18 @@
+function divisible(num, digit) {
+     return num % digit === 0
+}
+
+function inclusive(num, digit) {
+    return num.toString().indexOf(digit) > -1
+}
+
+
 function fizzBuzz(num) {
-    return num % 3 === 0 && num % 5 === 0 ? 'FizzBuzz' :
-    num % 5 === 0 && num.toString().indexOf('3') > -1 && num.toString().indexOf('5') > -1 ? 'FizzBuzzBuzz' :
-    num.toString().indexOf('3') > -1 && num.toString().indexOf('5') > -1 ? 'FizzBuzz' :
-    num % 3 === 0 || num.toString().indexOf('3') > -1 ? 'Fizz' :
-    num % 5 === 0 || num.toString().indexOf('5') > -1 ? 'Buzz' :
+    return divisible(num, 3) && divisible(num, 5) ? 'FizzBuzz' :
+    divisible(num, 5) && inclusive(num, 3) && inclusive(num, 5) ? 'FizzBuzzBuzz' :
+    inclusive(num, 3) && inclusive(num, 5) ? 'FizzBuzz' :
+    divisible(num, 3) || inclusive(num, 3) ? 'Fizz' :
+    divisible(num, 5) || inclusive(num, 5) ? 'Buzz' :
     num
 }
 
